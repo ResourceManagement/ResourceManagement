f2434bed06bde6e575d748091b6d3f12d0ba94b1
Pop Cristian <pop.danielcristian@yahoo.com>
2013-08-06 16:34:21 +0300

    Admin stuff

--- main.py
+++ main.py
@@ -25,13 +25,24 @@ import random
 import logging
 import time
 
+from string import letters
 from google.appengine.ext import db
 
 template_dir= os.path.join(os.path.dirname(__file__),'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
     autoescape=True)
 
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return username and USER_RE.match(username)
+PASS_RE = re.compile(r"^.{3,20}$")
+    
+def valid_password(password):
+    return password and PASS_RE.match(password)
 
+EMAIL_RE  = re.compile(r'^[\S]+@[\S]+\.[\S]+$')
+def valid_email(email):
+    return not email or EMAIL_RE.match(email)
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
@@ -64,6 +75,42 @@ class Handler(webapp2.RequestHandler):
         uid = self.read_secure_cookie('user_id')
         self.user = uid and User.by_id(int(uid))
 
+class CompanySignup(Handler):
+    def get(self):
+        self.render('add_company.html')
+
+    def post(self):
+        have_error = False
+        self.username = self.request.get('username_company')
+        self.password = self.request.get('password_company')
+        self.verify = self.request.get('verify_company')
+        self.email = self.request.get('email_company')
+
+        params = dict(username = self.username,
+                      email = self.email)
+
+        if not valid_username(self.username):
+            params['error_username'] = "That's not a valid username."
+            have_error = True
+
+        if not valid_password(self.password):
+            params['error_password'] = "That wasn't a valid password."
+            have_error = True
+        elif self.password != self.verify:
+            params['error_verify'] = "Your passwords didn't match."
+            have_error = True
+
+        if not valid_email(self.email):
+            params['error_email'] = "That's not a valid email."
+            have_error = True
+
+        if have_error:
+            self.render('add_company.html', **params)
+        else:
+            self.done()
+
+    def done(self, *a, **kw):
+        raise NotImplementedError
 class MainHandler(Handler):
     def get(self):
         self.render("main.html")
@@ -154,19 +201,40 @@ class User(db.Model):
         u = cls.by_name(name)
         if u and valid_pw(name, pw, u.pw_hash):
             return u
-
+    def render(self):
+        #self._render_text = self.content.replace('\n', '<br>')
+        return render_str("user.html", p = self)
         
         
 class TempHandler(Handler):
     def get(self):
         self.render('temp.html')
 
+class NewCompanyHandler(CompanySignup):
+    def done(self):
+        #make sure the user doesn't already exist
+        u = User.by_name(self.username)
+        if u:
+            msg = 'That user already exists.'
+            self.render('add_company.html', error_username = msg)
+        else:
+            u = User.register(self.username, self.password,'company', self.email)
+            u.put()
 
+            self.redirect('/admin')
+
+class ListCompaniesHandler(Handler):
+    def get(self):
+        companies = User.all().order('-name')
+        self.render('companies_list.html', companies = companies)
+        
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/login',Login),
     ('/admin',AdminHandler),
-    ('/admin/temp',TempHandler)
+    ('/admin/temp',TempHandler),
+    ('/admin/new',NewCompanyHandler),
+    ('/admin/list',ListCompaniesHandler)
 ], debug=True)
 
 
